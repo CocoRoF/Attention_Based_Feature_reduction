bbd310183c6f06e68a37cda16151bf6818a346d7
jinsookim <wlstn010203@gmail.com>
2023-08-25 16:34:35 +0900

    tsne 수정

--- util/t-SNE_attention.py
+++ util/t-SNE_attention.py
@@ -1,8 +1,7 @@
 import numpy as np
 from sklearn.manifold import TSNE
 
-# 비선형 차원축소방법 t-sne 로 만들어
-def softmax(x):
+def soft_max(x):
     exp_x = np.exp(x - np.max(x))
     return exp_x / exp_x.sum(axis=0, keepdims=True)
 
@@ -16,27 +15,33 @@ class TSNE_attention():
         self.dim_info = dim_info
         self.n_col = len(array[0])
         
-        # t-SNE를 사용해서 데이터를 저차원으로 변환
-        self.tsne_data = self.apply_tsne()
+        # t-SNE를 사용해서 데이터의 임베딩을 얻음
+        self.embeddings = self.apply_tsne()
         
-        # t-SNE로 변환된 데이터를 Query와 Key로 사용
-        self.attention_query = self.tsne_data[:dim_info]
-        self.attention_key = self.tsne_data[dim_info:2*dim_info]
+        # 얻어진 임베딩을 Query와 Key로 사용
+        self.attention_query = self.embeddings[:dim_info]
+        self.attention_key = self.embeddings[dim_info:2*dim_info]
         
-    def handle_missing_values(self, array):
-        return np.nan_to_num(array)  # NaN 값을 0으로 대체   
-    
     def apply_tsne(self):
-        tsne = TSNE(n_components=self.n_components)
+        if self.dim_info <= 4:
+            tsne = TSNE(n_components=self.dim_info)
+        else:
+            tsne = TSNE(n_components=self.dim_info, method='exact')
         return tsne.fit_transform(self.array)
-    
+
+
     def attention(self, threshold: bool = False, threshold_value: float = 0.1):
-        query_matrix = self.attention_query
-        key_matrix = self.attention_key
-        attention_score = np.apply_along_axis(softmax, axis=1, arr=(query_matrix.dot(key_matrix.T)))
+        query_matrix = self.embeddings[:self.dim_info]
+        key_matrix = self.embeddings[self.dim_info:2*self.dim_info]
+        
+        attention_score = query_matrix.dot(key_matrix.T)
+        attention_score = np.apply_along_axis(soft_max, axis=1, arr=attention_score)
         
         if threshold:
             return threshold_below(attention_score, threshold_value)
         else:
             return attention_score
+
+#Factor_attention에서는 loadings 값을 통해 attention을 계산하는 반면,
+#TSNE_attention에서는 t-SNE의 결과인 임베딩 값을 사용하여 attention을 계산하시면 됩니다.
 
